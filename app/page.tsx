3c5fa6b9e9b1221b94875d462f616bebd02b4420
pletumy <ple.tumy@gmail.com>
2024-09-09 15:28:10 +0700

    modify home pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 export default function Page() {
   return (
     <section>
-      <h1 className="font-medium text-2xl mb-8 tracking-tighter">my portfolio</h1>
+      <h1 className="font-medium text-2xl mb-8 tracking-tighter">AWS FCJ Bootcamp 2024</h1>
       <p className="prose prose-neutral dark:prose-invert">
-        This is your new portfolio.
+        A simple showcase by <a href="mailto:atran8262@gmail.com" className="text-blue-500 underline">An Tran</a> and <a href="mailto:ple.tumy@gmail.com" className="text-blue-500 underline">My Phan</a>. Click on "Blog" to view submitted workshops.
       </p>
     </section>
   );
